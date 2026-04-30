494c1a5a009cde552d2521b37d96097492792a4b
Justine Lhei Piol <tinerabaja308@gmail.com>
2026-04-30 13:37:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function toggleDarkMode(){
-document.body.classList.toggle("dark");
-
-const icon = document.querySelector(".dark-mode-toggle i");
-
-if(document.body.classList.contains("dark")){
-  icon.classList.remove("fa-moon");
-  icon.classList.add("fa-sun");
-} else{
-  icon.classList.remove("fa-sun");
-  icon.classList.add("fa-moon");
- }
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
 }
+
+window.addEventListener("scroll", function() {
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach(section => {
+    const position = section.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if(position < screenPosition){
+      section.style.opacity = 1;
+      section.style.transform = "translateY(0)";
+    }
+  });
+});
